0b41ba90b0c70ae97a8385d6f06438a70eb2feea
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-02-10 10:47:13 +0530

    update

--- controllers/Admincontroller/usercontroller.js
+++ controllers/Admincontroller/usercontroller.js
@@ -31,7 +31,7 @@ exports.reviewget = async (req, res) => {
 
 
 
-    res.render('Admin/Reviews', { reviewdata:data, page: 5,totalPages, currentPage})
+    res.render('Admin/Reviews', { reviewdata: data, page: 5, totalPages, currentPage })
 };
 
 
@@ -46,8 +46,8 @@ exports.UserGet = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = 5; // Set the number of products per page
     const skip = (page - 1) * limit;
-const demo = await UserCollection.find()
-// console.log(demo);
+    const demo = await UserCollection.find()
+    // console.log(demo);
     // Fetch products with pagination
     const data = await UserCollection.find()
         .skip(skip)
@@ -63,7 +63,7 @@ const demo = await UserCollection.find()
 
 
 
-    res.render("Admin/Users", { admin:data, page: 2, totalPages, currentPage })
+    res.render("Admin/Users", { admin: data, page: 2, totalPages, currentPage })
 };
 
 
@@ -76,7 +76,7 @@ exports.blockUser = async (req, res) => {
         const userId = req.query.userid;
         // console.log(userId);
         // Find user by ID      
-        const user = await UserCollection.findOne({_id:userId});
+        const user = await UserCollection.findOne({ _id: userId });
         if (!user) {
             console.log('User not found');
             // Handle the case where the user is not found
